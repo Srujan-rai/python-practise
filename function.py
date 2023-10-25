e8191eb90cad0927d3511c3d0e35c4e532657cf5
Srujan-rai <raisrujan@gmail.com>
2023-10-25 19:54:28 +0530

    python practise

---
+++ function.py
@@ -0,0 +1,8 @@
+def say_hi(name,age):
+    print("Hello "+name+" and the age is "+str(age))
+say_hi("srujan",17)
+
+def cube(num):
+    return num*num*num
+
+print(cube(3))
---
+++ lists.py
@@ -0,0 +1,16 @@
+lists=["srujan","rai","sru"]
+lists[1]="india"
+lucky_number=[1,2,3,4,5]
+print(lists[1])
+lists.extend(lucky_number)
+print(lists)
+lists.insert(1,"karnataka")
+print(lists)
+lists.remove("sru")
+print(lists)
+lists.pop()
+print(lists)
+
+friends=["sruj","rohan","deveesh","babith"]
+friends.sort()
+print(friends)
