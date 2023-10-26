4fd9d40c987d66989fc052b21cf122c725a17fe0
Srujan-rai <raisrujan@gmail.com>
2023-10-26 09:01:06 +0530

    python practise

---
+++ for_loop.py
@@ -0,0 +1,9 @@
+for letter in "india":
+    print(letter)
+friends=["srujan","rohan","deveesh"]
+for friend in friends:
+    print(friend)
+for i in range(20):
+    print(i)
+for i in range(100,200):
+    print(i)
