0249e8b47205093a5c1a5f219545a215fbb4e038
Srujan-rai <raisrujan@gmail.com>
2023-10-27 10:33:52 +0530

    python practise

--- translator.py
+++ translator.py
@@ -1,1 +1,15 @@
 #vowles ->g
+
+def translate(phrase):
+    tranlation = ""
+    for letter in phrase:
+        if letter.lower() in "aeiou":
+            if letter.isupper():
+                tranlation = tranlation + "G"
+            else:
+             tranlation=tranlation+"g"
+        else:
+            tranlation=tranlation+letter
+    return tranlation
+
+print(translate(input("enter the sentence:  ")))
