755c18200421c5c29acc8cb674eced606ab5d8c4
Srujan-rai <raisrujan@gmail.com>
2023-11-01 10:46:19 +0530

    python practise

---
+++ app.py
@@ -0,0 +1,8 @@
+from Student import Student  #1st one is the file and second one is the class name
+
+student1=Student("adi","business",3.1,False)
+student2=Student("rrohan","rich",3.1,False)
+student3=Student("deveesh","brilliant",3.1,False)
+
+print(student1.name)
+
